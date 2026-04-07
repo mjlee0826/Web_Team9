fb8567795803506a42b29d03c194aa212d1a9116
Karolin-lin <carol921011@gmail.com>
2026-04-08 01:19:57 +0800

    stop being stupid:)

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -1,1 +1,17 @@
-// TODO: 冠
+const todoService = require('./todo.service');
+
+exports.createTask = async (req, res) => {
+  try {
+    // 1. 從請求中拿取資料
+    const taskData = req.body; 
+    
+    // 2. 呼叫 Service 處理邏輯
+    const newTask = await todoService.createTodo(taskData);
+    
+    // 3. 回傳成功的結果
+    res.status(201).json(newTask);
+  } catch (error) {
+    // 4. 錯誤處理
+    res.status(400).json({ message: error.message });
+  }
+};
--- backend/src/routes/todoRoutes.js
+++ backend/src/routes/todoRoutes.js
@@ -1,6 +1,8 @@
-import { Router } from 'express';
+const express = require('express');
+const router = express.Router();
+const todoController = require('./todo.controller');
 
-const todoRouter = Router();
-// TODO: 冠
+// 當收到 POST /api/tasks 時，交給 controller 的 createTask 函式
+router.post('/tasks', todoController.createTask);
 
-export default todoRouter;
+module.exports = router;
